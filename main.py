aaa5dc7bf912b6ca6a2b6b6a752f262a8c0dd470
mullemi5 <mullemi5@fel.cvut.cz>
2020-10-21 18:15:35 +0200

    add pictures

--- main.py
+++ main.py
@@ -2,10 +2,11 @@ from Tkinter import *
 
 # creating a tk window
 root = Tk()
+root.title("SART test")
 
 # create label widget
 myLabel = Label(root, text="Hello world")
-# making it show in a pack way on the root
+# making it show in a pack way in the root
 myLabel.pack()
 
 # starting loop at root window
