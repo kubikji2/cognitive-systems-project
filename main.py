1de3e7ab77be758b3f3eea18d6c9a670b6c2f1a1
mullemi5 <mullemi5@fel.cvut.cz>
2020-10-21 18:51:16 +0200

    blank fullscreen app

--- main.py
+++ main.py
@@ -1,14 +1,34 @@
 from Tkinter import *
 
-# creating a tk window
-root = Tk()
-root.title("SART test")
 
-# create label widget
-myLabel = Label(root, text="Hello world")
-# making it show in a pack way in the root
-myLabel.pack()
+class MainWindow:
+    def __init__(self):
+        # init root window
+        self.window = Tk()
+        self.window.attributes('-fullscreen', True)
+        self.window.title("SART test")
+        self.window.tk_setPalette(background="black", foreground="white")
 
-# starting loop at root window
-root.mainloop()
+        # Bind keypresses
+        self.window.bind("<F11>",
+                         lambda event: self.window.attributes("-fullscreen", not self.window.attributes("-fullscreen")))
+        self.window.bind("<Escape>",
+                         lambda event: self.window.attributes("-fullscreen", False))
+
+        # Create widgets
+        myLabel = Label(self.window, text="Hello world")
+        # Add widgets
+        myLabel.pack()
+
+    def run(self):
+        self.window.mainloop()
+        # alternative
+        # while True:
+        #     root.update_idletasks()
+        #     root.update()
+
+
+if __name__ == '__main__':
+    app = MainWindow()
+    app.run()
 
