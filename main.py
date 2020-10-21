69b0ec4cf6f1e334ce0ace3ca1818ac15c6b96f5
mullemi5 <mullemi5@fel.cvut.cz>
2020-10-21 19:19:45 +0200

    fullscreen centered helloworld ESC -> quit F11 -> fullscreen toggle

--- main.py
+++ main.py
@@ -5,6 +5,8 @@ class MainWindow:
     def __init__(self):
         # init root window
         self.window = Tk()
+        self.window.geometry("800x600")
+        # self.window.eval('tk::PlaceWindow . center')  # place the windowed version to center of screen
         self.window.attributes('-fullscreen', True)
         self.window.title("SART test")
         self.window.tk_setPalette(background="black", foreground="white")
@@ -13,12 +15,15 @@ class MainWindow:
         self.window.bind("<F11>",
                          lambda event: self.window.attributes("-fullscreen", not self.window.attributes("-fullscreen")))
         self.window.bind("<Escape>",
-                         lambda event: self.window.attributes("-fullscreen", False))
+                         lambda event: self.window.destroy())
 
         # Create widgets
-        myLabel = Label(self.window, text="Hello world")
-        # Add widgets
-        myLabel.pack()
+        middle_frame = Frame(self.window)
+        my_label = Label(middle_frame, text="Hello world")
+
+        # Show widgets
+        middle_frame.place(relx=.5, rely=.5, anchor="center")
+        my_label.pack()
 
     def run(self):
         self.window.mainloop()
