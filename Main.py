2566a80400cb088f26a08c63e590d4e77fd52ebe
mullemi5 <mullemi5@fel.cvut.cz>
2020-10-21 17:18:08 +0200

    create a sample tkinter project using Pycharm and Python 2.7

---
+++ Main.py
@@ -0,0 +1,13 @@
+from Tkinter import *
+
+# creating a tk window
+root = Tk()
+
+# create label widget
+myLabel = Label(root, text="Hello world")
+# making it show in a pack way on the root
+myLabel.pack()
+
+# starting loop at root window
+root.mainloop()
+
